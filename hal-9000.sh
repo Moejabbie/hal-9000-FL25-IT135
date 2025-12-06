95a19e29ecfd23934dc89ca55fa2e40419b44d37
Moejabbie <129770937+Moejabbie@users.noreply.github.com>
2025-12-06 07:24:55 +0000

    Adds Hal program

---
+++ hal-9000.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+#A script to demo user input and variable use
+
+echo "Hell. I'm HAL 9000. What is your name?"
+
+read USERNAME
+
+echo "I'm sorry $USERNAME. I'm afraid i can't open the pod bay doors"
